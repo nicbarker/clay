48cb2f3951ab31e76fcd76bb6e6cc3a171f4e22f
Nic Barker <nic@nicbarker.com>
2024-09-28 21:50:06 +1200

    fix main.cpp

--- examples/cpp-project-example/main.cpp
+++ examples/cpp-project-example/main.cpp
@@ -4,7 +4,7 @@
 
 int main(void) {
     uint64_t totalMemorySize = Clay_MinMemorySize();
-    Clay_Arena clayMemory = (Clay_Arena) { .label = CLAY_STRING("Clay Memory Arena"), .capacity = totalMemorySize, .memory = (char *)malloc(totalMemorySize) };
-    Clay_Initialize(clayMemory, (Clay_Dimensions) {1024,768});
+    Clay_Arena clayMemory = Clay_Arena { .label = CLAY_STRING("Clay Memory Arena"), .capacity = totalMemorySize, .memory = (char *)malloc(totalMemorySize) };
+    Clay_Initialize(clayMemory, Clay_Dimensions {1024,768});
     return 0;
 }
