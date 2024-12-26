258993ad177843d6ac2b381b1fca4650aeb3fb93
Nic Barker <contact+github@nicbarker.com>
2024-12-26 19:46:36 +1300

    cpp fixes'

--- examples/cpp-project-example/main.cpp
+++ examples/cpp-project-example/main.cpp
@@ -12,7 +12,7 @@ void HandleClayErrors(Clay_ErrorData errorData) {
 int main(void) {
     uint64_t totalMemorySize = Clay_MinMemorySize();
     Clay_Arena clayMemory = Clay_CreateArenaWithCapacityAndMemory(totalMemorySize, (char *)malloc(totalMemorySize));
-    Clay_Initialize(clayMemory, Clay_Dimensions {1024,768}, (Clay_ErrorHandler) { HandleClayErrors });
+    Clay_Initialize(clayMemory, Clay_Dimensions {1024,768}, Clay_ErrorHandler { HandleClayErrors });
     Clay_BeginLayout();
     CLAY(CLAY_RECTANGLE({ .color = {255,255,255,0} }), CLAY_LAYOUT(layoutElement)) {}
     Clay_EndLayout();
