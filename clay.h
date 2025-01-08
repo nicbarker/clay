cc6dfccc7ef43ea3e4412f11c80bc49227c0619a
Nic Barker <contact+github@nicbarker.com>
2025-01-09 12:31:53 +1300

    update odin bindings and revert some accidental change s

--- clay.h
+++ clay.h
@@ -558,7 +558,7 @@ Clay_ScrollElementConfig * Clay__StoreScrollElementConfig(Clay_ScrollElementConf
 Clay_BorderElementConfig * Clay__StoreBorderElementConfig(Clay_BorderElementConfig config);
 Clay_ElementId Clay__HashString(Clay_String key, uint32_t offset, uint32_t seed);
 void Clay__OpenTextElement(Clay_String text, Clay_TextElementConfig *textConfig);
-uint32_t Clay__GetParentElementId();
+uint32_t Clay__GetParentElementId(void);
 
 extern Clay_Color Clay__debugViewHighlightColor;
 extern uint32_t Clay__debugViewWidth;
