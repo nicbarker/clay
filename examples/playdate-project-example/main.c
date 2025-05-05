ae27eddcabbfc132720d5a7c24364993a28bca76
Matthew Jennings <matt@mattahj.co.uk>
2025-05-05 19:39:23 +0300

    setup some interactivity with the dpad

--- examples/playdate-project-example/main.c
+++ examples/playdate-project-example/main.c
@@ -4,7 +4,7 @@
 #include "../../clay.h"
 
 #include "../../renderers/playdate/clay_renderer_playdate.c"
-#include "../shared-layouts/clay-video-demo.c"
+#include "clay-video-demo-playdate.c"
 
 static int update(void *userdata);
 const char *fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
@@ -17,18 +17,18 @@ struct TextUserData {
   PlaydateAPI *pd;
 };
 
-static struct TextUserData gTextUserData = {.font = NULL, .pd = NULL};
-static ClayVideoDemo_Data demoData;
+static struct TextUserData testUserData = {.font = NULL, .pd = NULL};
 
-static Clay_Dimensions PlayDate_MeasureText(Clay_StringSlice text,
-                                            Clay_TextElementConfig *config,
-                                            void *userData) {
-  // TODO: playdate needs to load fonts at the given size, so need to do that
-  // before we can use different font sizes!
+static Clay_Dimensions PlayDate_MeasureText(Clay_StringSlice text, Clay_TextElementConfig *config, void *userData) {
+  // TODO: playdate needs to load fonts at the given size, so need to do that before we can use different font sizes!
   struct TextUserData *textUserData = userData;
   int width = textUserData->pd->graphics->getTextWidth(
-      textUserData->font, text.chars,
-      utf8_count_codepoints(text.chars, text.length), kUTF8Encoding, 0);
+    textUserData->font,
+    text.chars,
+    utf8_count_codepoints(text.chars, text.length),
+    kUTF8Encoding,
+    0
+  );
   int height = textUserData->pd->graphics->getFontHeight(textUserData->font);
   return (Clay_Dimensions){
       .width = (float)width,
@@ -39,65 +39,66 @@ static Clay_Dimensions PlayDate_MeasureText(Clay_StringSlice text,
 #ifdef _WINDLL
 __declspec(dllexport)
 #endif
-int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
+int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t eventArg)
 {
-  (void)arg; // arg is currently only used for event = kEventKeyPressed
-
   if (event == kEventInit) {
     const char *err;
     font = pd->graphics->loadFont(fontpath, &err);
 
-    if (font == NULL)
-      pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__,
-                        fontpath, err);
+    if (font == NULL) {
+      pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
+    }
 
-    gTextUserData.pd = pd;
-    gTextUserData.font = font;
-
-    // Note: If you set an update callback in the kEventInit handler, the system
-    // assumes the game is pure C and doesn't run any Lua code in the game
+    testUserData.pd = pd;
+    testUserData.font = font;
     pd->system->setUpdateCallback(update, pd);
 
     uint64_t totalMemorySize = Clay_MinMemorySize();
-    Clay_Arena clayMemory = Clay_CreateArenaWithCapacityAndMemory(
-        totalMemorySize, pd->system->realloc(NULL, totalMemorySize));
-    Clay_Initialize(clayMemory,
-                    (Clay_Dimensions){(float)pd->display->getWidth(),
-                                      (float)pd->display->getHeight()},
-                    (Clay_ErrorHandler){HandleClayErrors});
-    Clay_SetMeasureTextFunction(PlayDate_MeasureText, &gTextUserData);
-    demoData = ClayVideoDemo_Initialize(pd);
+    Clay_Arena clayMemory = Clay_CreateArenaWithCapacityAndMemory(totalMemorySize, pd->system->realloc(NULL, totalMemorySize));
+    Clay_Initialize(
+      clayMemory,
+      (Clay_Dimensions){(float)pd->display->getWidth(), (float)pd->display->getHeight()},
+      (Clay_ErrorHandler){HandleClayErrors}
+    );
+    Clay_SetMeasureTextFunction(PlayDate_MeasureText, &testUserData);
+    ClayVideoDemo_Initialize();
   }
 
   return 0;
 }
 
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400 - TEXT_WIDTH) / 2;
-int y = (240 - TEXT_HEIGHT) / 2;
-int dx = 1;
-int dy = 2;
+int selectedDocumentIndex = 0;
+#define WRAP_RANGE(x, N) ((((x) % (N)) + (N)) % (N))
 
 static int update(void *userdata) {
   PlaydateAPI *pd = userdata;
+  PDButtons pushedButtons;
+  pd->system->getButtonState(NULL, &pushedButtons, NULL);
+
+  if (pushedButtons & kButtonDown) {
+    selectedDocumentIndex = WRAP_RANGE(selectedDocumentIndex + 1, MAX_DOCUMENTS);
+  } else if (pushedButtons & kButtonUp) {
+    selectedDocumentIndex = WRAP_RANGE(selectedDocumentIndex - 1, MAX_DOCUMENTS);
+  }
 
   pd->graphics->clear(kColorWhite);
   pd->graphics->setFont(font);
 
-  Clay_SetPointerState((Clay_Vector2){.x = pd->display->getWidth() / 2.0f,
-                                      .y = pd->display->getHeight() / 2.0f},
-                       false);
+  // A bit hacky, setting the cursor on to the document view so it can be
+  // scrolled..
+  Clay_SetPointerState((Clay_Vector2){
+    .x = pd->display->getWidth() / 2.0f,
+    .y = pd->display->getHeight() / 2.0f
+  }, false);
   float crankDelta = pd->system->getCrankChange();
-  Clay_UpdateScrollContainers(true, (Clay_Vector2){0, crankDelta * 0.25f},
-                              pd->system->getElapsedTime());
-  Clay_RenderCommandArray renderCommands =
-      ClayVideoDemo_CreateLayout(&demoData);
+  Clay_UpdateScrollContainers(
+    false,
+    (Clay_Vector2){0, -crankDelta * 0.25f},
+    pd->system->getElapsedTime()
+  );
+  Clay_RenderCommandArray renderCommands = ClayVideoDemo_CreateLayout(selectedDocumentIndex);
 
   Clay_Playdate_Render(pd, renderCommands, font);
-
-  pd->system->drawFPS(0, 0);
 
   return 1;
 }
--- renderers/playdate/clay_renderer_playdate.c
+++ renderers/playdate/clay_renderer_playdate.c
@@ -28,7 +28,10 @@ static size_t utf8_count_codepoints(const char *str, size_t byte_len) {
 }
 
 static LCDColor clayColorToLCDColor(Clay_Color color) {
-  if (color.r == 255 && color.g == 255 && color.b == 255) {
+  // Convert the RGB to grayscale using the standard luminance formula
+  unsigned char grayscale_value = (unsigned char)(0.299 * color.r + 0.587 * color.g + 0.114 * color.b);
+
+  if (grayscale_value > 128) {
     return kColorWhite;
   }
   return kColorBlack;
@@ -42,27 +45,26 @@ static LCDBitmapDrawMode clayColorToDrawMode(Clay_Color color) {
   return kDrawModeCopy;
 }
 
-static void Clay_Playdate_Render(PlaydateAPI *pd,
-                                 Clay_RenderCommandArray renderCommands,
-                                 LCDFont *fonts) {
+static void Clay_Playdate_Render(PlaydateAPI *pd, Clay_RenderCommandArray renderCommands, LCDFont *fonts) {
 
   for (uint32_t i = 0; i < renderCommands.length; i++) {
-    Clay_RenderCommand *renderCommand =
-        Clay_RenderCommandArray_Get(&renderCommands, i);
+    Clay_RenderCommand *renderCommand = Clay_RenderCommandArray_Get(&renderCommands, i);
     Clay_BoundingBox boundingBox = renderCommand->boundingBox;
     switch (renderCommand->commandType) {
     case CLAY_RENDER_COMMAND_TYPE_RECTANGLE: {
       Clay_RectangleRenderData *config = &renderCommand->renderData.rectangle;
       struct Rect rect = (struct Rect){
-          .x = boundingBox.x,
-          .y = boundingBox.y,
-          .w = boundingBox.width,
-          .h = boundingBox.height,
+        .x = boundingBox.x,
+        .y = boundingBox.y,
+        .w = boundingBox.width,
+        .h = boundingBox.height,
       };
       if (config->cornerRadius.topLeft > 0) {
         pd->graphics->fillRoundRect(
-            rect.x, rect.y, rect.w, rect.h, config->cornerRadius.topLeft,
-            clayColorToLCDColor(config->backgroundColor));
+          rect.x, rect.y, rect.w, rect.h,
+          config->cornerRadius.topLeft,
+          clayColorToLCDColor(config->backgroundColor)
+        );
       } else {
         pd->graphics->fillRect(rect.x, rect.y, rect.w, rect.h,
                                clayColorToLCDColor(config->backgroundColor));
@@ -76,32 +78,36 @@ static void Clay_Playdate_Render(PlaydateAPI *pd,
       // layout has..
       LCDFont *font = fonts;
       struct Rect destination = (struct Rect){
-          .x = boundingBox.x,
-          .y = boundingBox.y,
-          .w = boundingBox.width,
-          .h = boundingBox.height,
+        .x = boundingBox.x,
+        .y = boundingBox.y,
+        .w = boundingBox.width,
+        .h = boundingBox.height,
       };
       pd->graphics->setDrawMode(clayColorToDrawMode(config->textColor));
       pd->graphics->drawText(
+        renderCommand->renderData.text.stringContents.chars,
+        utf8_count_codepoints(
           renderCommand->renderData.text.stringContents.chars,
-          utf8_count_codepoints(
-              renderCommand->renderData.text.stringContents.chars,
-              renderCommand->renderData.text.stringContents.length),
-          kUTF8Encoding, destination.x, destination.y);
+          renderCommand->renderData.text.stringContents.length
+        ),
+        kUTF8Encoding,
+        destination.x,
+        destination.y
+      );
       pd->graphics->setDrawMode(kDrawModeCopy);
-
       break;
     }
     case CLAY_RENDER_COMMAND_TYPE_SCISSOR_START: {
       struct Rect currentClippingRectangle = (struct Rect){
-          .x = boundingBox.x,
-          .y = boundingBox.y,
-          .w = boundingBox.width,
-          .h = boundingBox.height,
+        .x = boundingBox.x,
+        .y = boundingBox.y,
+        .w = boundingBox.width,
+        .h = boundingBox.height,
       };
       pd->graphics->setClipRect(
-          currentClippingRectangle.x, currentClippingRectangle.y,
-          currentClippingRectangle.w, currentClippingRectangle.h);
+        currentClippingRectangle.x, currentClippingRectangle.y,
+        currentClippingRectangle.w, currentClippingRectangle.h
+      );
       break;
     }
     case CLAY_RENDER_COMMAND_TYPE_SCISSOR_END: {
@@ -111,44 +117,43 @@ static void Clay_Playdate_Render(PlaydateAPI *pd,
     case CLAY_RENDER_COMMAND_TYPE_IMAGE: {
       Clay_ImageRenderData *config = &renderCommand->renderData.image;
       LCDBitmap *texture = config->imageData;
-
       struct Rect destination = (struct Rect){
-          .x = boundingBox.x,
-          .y = boundingBox.y,
-          .w = boundingBox.width,
-          .h = boundingBox.height,
+        .x = boundingBox.x,
+        .y = boundingBox.y,
+        .w = boundingBox.width,
+        .h = boundingBox.height,
       };
-
-      pd->graphics->drawBitmap(texture, destination.x, destination.y,
-                               kBitmapUnflipped);
-
+      pd->graphics->drawBitmap(texture, destination.x, destination.y, kBitmapUnflipped);
       break;
     }
     case CLAY_RENDER_COMMAND_TYPE_BORDER: {
       Clay_BorderRenderData *config = &renderCommand->renderData.border;
-
       struct Rect rect = (struct Rect){
-          .x = boundingBox.x,
-          .y = boundingBox.y,
-          .w = boundingBox.width,
-          .h = boundingBox.height,
+        .x = boundingBox.x,
+        .y = boundingBox.y,
+        .w = boundingBox.width,
+        .h = boundingBox.height,
       };
 
       // TODO: properly support the different border thickness and radius
       // instead of just using topLeft corner /top thickness as a global setting
       if (config->cornerRadius.topLeft > 0) {
         pd->graphics->drawRoundRect(
-            rect.x, rect.y, rect.w, rect.h, config->cornerRadius.topLeft,
-            config->width.top, clayColorToLCDColor(config->color));
+          rect.x, rect.y, rect.w, rect.h,
+          config->cornerRadius.topLeft,
+          config->width.top,
+          clayColorToLCDColor(config->color)
+        );
       } else {
-        pd->graphics->drawRect(rect.x, rect.y, rect.w, rect.h,
-                               clayColorToLCDColor(config->color));
+        pd->graphics->drawRect(
+          rect.x, rect.y, rect.w, rect.h,
+          clayColorToLCDColor(config->color)
+        );
       }
       break;
     }
     default: {
-      pd->system->logToConsole("Error: unhandled render command: %d\n",
-                               renderCommand->commandType);
+      pd->system->logToConsole("Error: unhandled render command: %d\n", renderCommand->commandType);
       return;
     }
     }
