e93a83812ed33341e4f08fd58d6dbfdf5989fd80
Nic Barker <contact+github@nicbarker.com>
2025-05-21 13:35:10 +1200

    Update to latest clay API, reuse example and fix a couple of small issues

--- examples/terminal-example/main.c
+++ examples/terminal-example/main.c
@@ -3,55 +3,37 @@
 
 #include <unistd.h>
 #include "../../clay.h"
-#include "../../renderers/terminal/clay_renderer_terminal.c"
+#include "../../renderers/terminal/clay_renderer_terminal_ansi.c"
+#include "../shared-layouts/clay-video-demo.c"
 
 const Clay_Color COLOR_LIGHT = (Clay_Color) {224, 215, 210, 255};
 const Clay_Color COLOR_RED = (Clay_Color) {168, 66, 28, 255};
 const Clay_Color COLOR_ORANGE = (Clay_Color) {225, 138, 50, 255};
 
-// An example function to begin the "root" of your layout tree
-Clay_RenderCommandArray CreateLayout() {
-	Clay_BeginLayout();
-
-	CLAY(CLAY_ID("OuterContainer"),
-	     CLAY_LAYOUT({.layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}, }),
-	     CLAY_RECTANGLE({ .color = {0,0,0,255} })) {
-		CLAY(CLAY_ID("SideBar"),
-		     CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_PERCENT(
-			     0.5), .height = CLAY_SIZING_PERCENT(1)}}),
-		     CLAY_RECTANGLE({.color = (Clay_Color) {255, 255, 255, 255}})
-		) {
-		}
-		CLAY(CLAY_ID("OtherSideBar"),
-		     CLAY_LAYOUT({.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_PERCENT(
-			     0.5), .height = CLAY_SIZING_PERCENT(1)}}),
-		     CLAY_RECTANGLE({ .color = {0,0, 0, 255 }})
-		) {
-			// TODO font size is wrong, only one is allowed, but I don't know which it is
-			CLAY_TEXT(CLAY_STRING("0123456789 0123456 78901 234567 89012 34567 8901234567890 123456789"),
-			          CLAY_TEXT_CONFIG({ .fontId = 0, .fontSize = 24, .textColor = {255,255,255,255} }));
-		}
-	}
-
-	return Clay_EndLayout();
+void HandleClayErrors(Clay_ErrorData errorData) {
+    printf("%s", errorData.errorText.chars);
 }
 
 int main() {
-	const int width = 80;
-	const int height = 24;
+    const int width = 145;
+    const int height = 41;
+    int columnWidth = 16;
 
-	uint64_t totalMemorySize = Clay_MinMemorySize();
-	Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(totalMemorySize, malloc(totalMemorySize));
-	Clay_Initialize(arena, (Clay_Dimensions) { .width = (float) width, .height = (float) height }); // TODO this is wrong, but I have no idea what the actual size of the terminal is in pixels
-	// Tell clay how to measure text
-	Clay_SetMeasureTextFunction(Console_MeasureText);
+    uint64_t totalMemorySize = Clay_MinMemorySize();
+    Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(totalMemorySize, malloc(totalMemorySize));
+    Clay_Initialize(arena,
+                    (Clay_Dimensions) {.width = (float) width * columnWidth, .height = (float) height * columnWidth},
+                    (Clay_ErrorHandler) {HandleClayErrors});
+    // Tell clay how to measure text
+    Clay_SetMeasureTextFunction(Console_MeasureText, &columnWidth);
+    ClayVideoDemo_Data demoData = ClayVideoDemo_Initialize();
 
-	while(true) {
-		Clay_RenderCommandArray layout = CreateLayout();
+    while (true) {
+        Clay_RenderCommandArray renderCommands = ClayVideoDemo_CreateLayout(&demoData);
 
-		Clay_Console_Render(layout, width, height);
+        Clay_Console_Render(renderCommands, width, height, columnWidth);
 
-		fflush(stdout);
-		sleep(1);
-	}
+        fflush(stdout);
+        sleep(1);
+    }
 }
