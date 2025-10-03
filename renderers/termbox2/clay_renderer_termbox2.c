cad93c4ff516a36b72e5785063556f03e243b1eb
Daniel Mayovskiy <comepot.4anell@gmail.com>
2025-10-02 19:50:27 -0700

    code alteration license compliance

--- renderers/termbox2/clay_renderer_termbox2.c
+++ renderers/termbox2/clay_renderer_termbox2.c
@@ -3,6 +3,8 @@
 
     Copyright (c) 2025 Mivirl
 
+    altered by Godje (Sep 2025)
+
     This software is provided 'as-is', without any express or implied warranty.
     In no event will the authors be held liable for any damages arising from the
     use of this software.
